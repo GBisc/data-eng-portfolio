9dfc2cd270d917ba40f5569ec78f2a821d9f69ac
liam <liamkalita@googlemail.com>
2025-07-21 13:42:22 +0100

    Get a few pokemon and print

--- pokemon-api-read/pokemon_data_read.py
+++ pokemon-api-read/pokemon_data_read.py
@@ -1,8 +1,19 @@
 import requests
 
-# Get one Pokemon and print it
-response = requests.get('https://pokeapi.co/api/v2/pokemon/1')
-pokemon_data = response.json()
-print(f"Pokemon: {pokemon_data['name']}")
-print(f"Height: {pokemon_data['height']}")
-print(f"Weight: {pokemon_data['weight']}")
+pokemon_list = []
+
+for pokemon_id in range(1, 6):
+    print(f"Fetching Pokemon #{pokemon_id}...")
+    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}')
+    pokemon_data = response.json()
+    
+    simplified_pokemon = {
+        'id': pokemon_data['id'],
+        'name': pokemon_data['name'],
+        'height': pokemon_data['height'],
+        'weight': pokemon_data['weight']
+    }
+    pokemon_list.append(simplified_pokemon)
+
+for pokemon in pokemon_list:
+    print(pokemon)
