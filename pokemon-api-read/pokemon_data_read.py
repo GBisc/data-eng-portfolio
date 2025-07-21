e23c58f489f204a75b25f4b89c290804fde56547
liam <liamkalita@googlemail.com>
2025-07-21 13:49:44 +0100

    export to csv using pandas

--- pokemon-api-read/pokemon_data_read.py
+++ pokemon-api-read/pokemon_data_read.py
@@ -1,8 +1,10 @@
 import requests
+import pandas as pd
 
 pokemon_list = []
+first_x_pokemon = 10
 
-for pokemon_id in range(1, 6):
+for pokemon_id in range(1, (first_x_pokemon + 1)):
     print(f"Fetching Pokemon #{pokemon_id}...")
     response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}')
     pokemon_data = response.json()
@@ -11,9 +13,12 @@ for pokemon_id in range(1, 6):
         'id': pokemon_data['id'],
         'name': pokemon_data['name'],
         'height': pokemon_data['height'],
-        'weight': pokemon_data['weight']
+        'weight': pokemon_data['weight'],
+        'base_experience': pokemon_data['base_experience']
     }
     pokemon_list.append(simplified_pokemon)
 
-for pokemon in pokemon_list:
-    print(pokemon)
+df = pd.DataFrame(pokemon_list)
+df.to_csv('pokemon_basic.csv', index=False)
+print(f"Saved {len(df)} Pokemon to pokemon_basic.csv")
+print(df.head())
